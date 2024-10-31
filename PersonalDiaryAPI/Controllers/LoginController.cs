bf8d20863b6ed597d3cffaa2366714174ee0fcdc
HaLBH <144716364+HaLBH176637@users.noreply.github.com>
2024-10-31 16:23:06 +0700

    Add Authorize

---
+++ PersonalDiaryAPI/Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using PersonalDiaryAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace PersonalDiaryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        private PersonalDiaryContext _context;
+        public LoginController(IConfiguration configuration, PersonalDiaryContext context)
+        {
+            _context = context;
+            _config = configuration;
+        }
+        private User AuthenticateUser(string username, string password)
+        {
+            User user = _context.Users.FirstOrDefault(x => x.Username.Equals(username) && x.Password.Equals(password));
+            return user; 
+        }
+
+        private string GenerateToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                null,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        [AllowAnonymous]
+        [HttpPost("Authenticate")]
+        public IActionResult Login(string username, string password)
+        {
+            IActionResult response = Unauthorized();
+            var user = AuthenticateUser(username, password);
+            if (user != null)
+            {
+                var token = GenerateToken(user);
+                response = Ok(new { token = token });
+            }
+            return response;
+        }
+    }
+}
---
+++ PersonalDiaryAPI/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PersonalDiaryAPI.Models;
+
+namespace PersonalDiaryAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private IConfiguration _config;
+        private PersonalDiaryContext _context;
+        public UserController(IConfiguration configuration, PersonalDiaryContext context)
+        {
+            _context = context;
+            _config = configuration;
+        }
+        [HttpGet]
+        [Route("GetUser")]
+        public IActionResult GetUser()
+        {
+            var user = _context.Users.ToList();
+            return Ok(user);
+        }
+    }
+}
