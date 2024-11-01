64d3e4b22f8a86d34627feb9528c7710b3315ef3
HaLBH <144716364+HaLBH176637@users.noreply.github.com>
2024-11-01 17:05:12 +0700

    API User

---
+++ PersonalDiaryAPI/Controllers/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace PersonalDiaryAPI.Controllers
+{
+    public class LoginDTO
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
